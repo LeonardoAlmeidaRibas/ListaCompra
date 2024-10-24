0e6731385449851ed85f86375558a81a18b1fcae
LeonardoAlmeidaRibas <leo.almeidaribas21@gmail.com>
2024-10-24 18:51:31 -0300

    Agora feito

--- src/controllers/compras-controller.js
+++ src/controllers/compras-controller.js
@@ -16,3 +16,30 @@ export const show = async (req, res) => {
         res.status(400).send(error);
     }
 };
+
+export const index = async (req, res) => {
+    try {
+        const compras = await Compras.find().exec();
+        return res.status(200).json(compras);
+    } catch (error) {
+        return res.status(500).json({ error: error });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const compras = await Compras.findByIdAndUpdate(req.params.id, req.body).exec();
+        return res.status(200).json(compras);
+    } catch (error) {
+        return res.status(500).json({ error: error });
+    }
+};
+
+export const destroy = async (req, res) => {
+    try {
+        const compras = await Compras.findByIdAndDelete(req.params.id).exec();
+        return res.status(204).json();
+    } catch (error) {
+        return res.status(500).json({ error: error });
+    }
+};
--- src/routes/compras-router.js
+++ src/routes/compras-router.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import {
     store,
-    show
+    show,
+    index,
+    update,
+    destroy
 } from '../controllers/compras-controller.js';
 
 const router = Router();
 
 router.post('/', store);
 router.get("/:id", show);
+router.get('/', index);
+router.put('/:id', update);
+router.delete('/:id', destroy);
 
 export default router;
